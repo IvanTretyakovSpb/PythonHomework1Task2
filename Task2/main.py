f4893ea4b6d9a1bb585d54d704dc334003828040
Tretyakov Ivan <timmitarh@mail.ru>
2023-05-22 13:01:30 +0300

    Completed task 2

---
+++ Task2/main.py
@@ -0,0 +1,12 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+# *Пример:*
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+number = int(input('Введите целое трёхзначное число: '))
+digit1 = number // 100 
+digit2 = number // 10 % 10
+digit3 = number % 10
+sum = digit1 + digit2 + digit3
+print(f'{number} -> {sum} ({digit1} + {digit2} + {digit3})')
